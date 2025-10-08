0e984c80ba18b1d911f464b7e0e1a7e61d5f8417
Oliwier-A <120788476+Oliwier-A@users.noreply.github.com>
2025-10-08 17:51:53 +0200

    added comments and replies models

--- Models/Interactions/Comments/Comment.cs
+++ Models/Interactions/Comments/Comment.cs
@@ -1,6 +1,29 @@
-﻿namespace praca_dyplomowa_zesp.Models.Interactions.Comments
+﻿using praca_dyplomowa_zesp.Models.Users;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.Extensions.Hosting;
+using praca_dyplomowa_zesp.Models.Interactions.Comments.Replies;
+
+namespace praca_dyplomowa_zesp.Models.Interactions.Comments
 {
     public class Comment
     {
+            [Key]
+            public Guid Id { get; set; } = Guid.NewGuid();
+
+            [Required]
+            [MaxLength(2000)]
+            public string Content { get; set; } = string.Empty;
+
+            [Required]
+            public Guid UserId { get; set; }
+
+            public User User { get; set; } = null!;
+
+            public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+            public DateTime? UpdatedAt { get; set; }
+
+            public ICollection<Reply> Replies { get; set; } = new List<Reply>();
     }
+    
 }
---
+++ Models/Interactions/Comments/Replies/Reply.cs
@@ -0,0 +1,27 @@
+﻿using praca_dyplomowa_zesp.Models.Users;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace praca_dyplomowa_zesp.Models.Interactions.Comments.Replies
+{
+    public class Reply
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        [Required]
+        [MaxLength(2000)]
+        public string Content { get; set; } = string.Empty;
+
+        [Required]
+        public Guid UserId { get; set; }
+
+        public User User { get; set; } = null!;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? UpdatedAt { get; set; }
+        public Guid CommentId { get; set; }
+        public Comment Comment { get; set; } = null!;
+    }    
+}
